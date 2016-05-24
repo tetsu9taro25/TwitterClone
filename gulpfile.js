a089ad76393d9809bb6aeab146a3a686a4e24778
Tetsutaro Endo <tetsutaro.endo@gaiax.com>
2016-05-24 15:27:47 +0900

    gulpがこのプロジェクトで動くように変更

--- gulpfile.js
+++ gulpfile.js
@@ -11,44 +11,37 @@ var gulp = require('gulp')
   , jshint = require('gulp-jshint')
   , stylish = require('jshint-stylish')
   , sass = require('gulp-sass')
-  , slim = require('gulp-slim')
   , coffee = require('gulp-coffee')
   , prefix = require('gulp-autoprefixer')
   , csslint = require('gulp-csslint')
   , htmlhint = require('gulp-htmlhint')
   , plumber = require('gulp-plumber')
-  , notify = require('gulp-notify');
-
+  , notify = require('gulp-notify')
+  , indirection = 'static';
 /**
  * Paths
  */
 var paths = {
   scripts: [
-    'htdocs/**/_*.js'
+    indirection + '/**/_*.js'
   ],
   jshint: [
     'gulpfile.js',
-    'htdocs/**/*.js',
-    '!htdocs/**/all.min.js',
-    '!htdocs/**/vendor/**/*.js'
+    indirection + '/**/*.js',
+    '!' + indirection + '/**/all.min.js',
+    '!' + indirection + '/**/vendor/**/*.js',
+    '!' + indirection + '/**/bootstrap/**/*.js'
   ],
   styles: [
-    'htdocs/**/*.scss',
-    '!htdocs/**/_*.scss'
-  ],
-  slim: [
-    'htdocs/**/*.slim',
-    '!htdocs/**/includes/*.slim'
+    indirection + '/**/*.scss',
+    '!' + indirection + '/**/_*.scss'
   ],
   htmlhint: [
-    'htdocs/*.html',
+    indirection + '/*.html',
   ],
   watch: {
     styles: [
-      'htdocs/**/*.scss'
-    ],
-    slim: [
-      'htdocs/**/*.slim'
+      indirection + '/**/*.scss'
     ]
   }
 };
@@ -65,7 +58,7 @@ var options = {
   },
   styles: {
     sass: {
-      paths: ['/htdocs/**/scss']
+      paths: [indirection + '/**/scss']
     },
     prefix: ['> 1%', 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'ios 6', 'android 4']
   }
@@ -80,7 +73,7 @@ gulp.task('scripts', function() {
     .pipe(plumber())
     .pipe(concat('all.min.js'))
     .pipe(uglify(options.scripts.uglify))
-    .pipe(gulp.dest('htdocs/common/js/'));
+    .pipe(gulp.dest(indirection + '/common/js/'));
 });
 
 /**
@@ -108,27 +101,9 @@ gulp.task('styles', function() {
     .pipe(rename(function(data) {
       data.dirname = path.join(data.dirname, '..', 'css');
     }))
-    .pipe(gulp.dest('htdocs/'))
+    .pipe(gulp.dest(indirection + '/'))
     .pipe(csslint('.csslintrc'))
     .pipe(csslint.reporter());
-});
-
-/**
- * Compile slim
- */
-gulp.task('slim', function() {
-  gulp
-    .src(paths.slim)
-    .pipe(plumber())
-    .pipe(slim({
-      pretty: true,
-      require: 'slim/include',
-      options: 'include_dirs=["htdocs/includes"]'
-    }))
-    .pipe(gulp.dest('htdocs/'))
-    .pipe(htmlhint('.htmlhintrc'))
-    .pipe(htmlhint.failReporter())
-    .on('error', notify.onError());
 });
 
 /**
@@ -147,7 +122,7 @@ gulp.task('htmlhint', function() {
 /**
  * Task dependencies.
  */
-gulp.task('all', ['scripts', 'jshint', 'styles', 'slim', 'htmlhint']);
+gulp.task('all', ['scripts', 'jshint', 'styles', 'htmlhint']);
 
 /**
  * Watch.
@@ -156,8 +131,7 @@ gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.jshint, ['jshint']);
   gulp.watch(paths.watch.styles, ['styles']);
-  gulp.watch(paths.watch.slim,['slim']);
-  gulp.watch(paths.watch.slim,['htmlhint']);
+  gulp.watch(paths.watch.html,['htmlhint']);
 });
 
 /**
