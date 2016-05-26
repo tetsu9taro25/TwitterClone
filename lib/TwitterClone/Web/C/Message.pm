f1dd96aeb40247f196f1d8febd2724a839e6740c
Tetsutaro Endo <tetsu9taro25@gmail.com>
2016-05-27 03:40:29 +0900

    プロフィール変更にもセッションを適応

--- lib/TwitterClone/Web/C/Message.pm
+++ lib/TwitterClone/Web/C/Message.pm
@@ -15,7 +15,7 @@ sub create {
   my $text = $c->req->parameters->{text}
     or return $c->res_404;
 
-  my $id = TwitterClone::Repository::Message->create($user_id, $text);
+  my $message_id = TwitterClone::Repository::Message->create($user_id, $text);
 
   return $c->redirect("/discover");
 }
@@ -35,9 +35,12 @@ sub edit {
 
 sub delete {
   my ($class, $c, $args) = @_;
-  my $id = $args->{id};
+  my $message_id = $args->{id};
+  my $user_id = $c->session->get('user_id') or return $c->redirect("/login");
+  my $old_message = TwitterClone::Repository::Message->fetch_by_id($message_id) or return $c->res_404;
+  TwitterClone::Repository::Message->validate($user_id, $old_message->user_id) or return $c->res_403;
 
-  TwitterClone::Repository::Message->delete($id);
+  TwitterClone::Repository::Message->delete($message_id);
 
   return $c->redirect("/discover");
 }
--- lib/TwitterClone/Web/C/Profile.pm
+++ lib/TwitterClone/Web/C/Profile.pm
@@ -8,7 +8,7 @@ use TwitterClone::Repository::Profile;
 
 sub new {
   my ($class, $c, $args) = @_;
-  my $user_id = 3; #セッションから引っ張ってくる
+  my $user_id = $c->session->get('user_id') or return $c->redirect("/login");
   my $user_data = TwitterClone::Repository::Profile->fetch_by_user_id($user_id);
 
   return $c->render('profile.tx', {
@@ -20,10 +20,13 @@ sub new {
 
 sub update {
   my ($class, $c, $args) = @_;
+  my $user_id = $c->session->get('user_id') or return $c->redirect("/login");
+  my $user_data = TwitterClone::Repository::Profile->fetch_by_user_id($user_id);
   my $image_path = TwitterClone::Repository::Profile->image_upload($c->req->uploads->{'image'}, $c->base_dir());
+
   my %user_data = (
-    screen_name => 'fa',#セッションから引っ張ってくる
-    id => 3, #セッションから引っ張ってくる
+    screen_name => $user_data->screen_name,
+    id => $user_id,
     name => $c->req->parameters->{name},
     password => $c->req->parameters->{password},
     mail => $c->req->parameters->{mail},
